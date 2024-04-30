e5a3d28294e618f2162f0f07d0b78fe3dfe34c4c
Adham Radwan <aradw009@uottawa.ca>
2024-04-30 19:35:57 -0400

    Changed model to use the trained model from the dataset and disregarded the output video for their large size

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,9 @@
 # Use this file to play around with YOLO using ultralytics
 from ultralytics import YOLO
 
-#load model
-model = YOLO('yolov8x')
+#load trained model
+# model = YOLO('yolo8x')
+model = YOLO('models/best.pt')
 
 #run the model on the input video and save the output of the results of the prediction back to the results variable
 results = model.predict('input_videos/08fd33_4.mp4',save=True)
