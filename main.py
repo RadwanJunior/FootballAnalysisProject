8b1233af181c68368a3a8571b6bdcce0e9e52233
Adham Radwan <aradw009@uottawa.ca>
2024-05-04 15:35:47 -0400

    Commented out code to get 1 cropped image of a player to do color segmentation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
 
 def main():
     #Read Video
@@ -12,6 +13,18 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+    
+    # #Try to crop and save an image of a player
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player['bbox']
+    #     frame = video_frames[0]
+
+    #     #crop bbox from frame, get cropped image and then the frame. Start from y1 to y2 and from x1 to x2
+    #     cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+    #     #save cropped image
+    #     cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+    #     break
     
     #Draw output
     ##Draw object tracks
